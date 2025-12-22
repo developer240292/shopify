ad0487c13c5b3d0f7132632fbbcf2c46dc6bdc22
Nguyen Dinh Hoang <hoangnd@villatheme.com>
2025-12-22 17:19:29 +0700

    theme

--- assets/media-gallery.js
+++ assets/media-gallery.js
@@ -18,7 +18,6 @@ if (!customElements.get('media-gallery')) {
     }
 
     onSlideChanged(event) {
-      console.log(event.detail.currentElement.dataset.mediaId);
       const thumbnail = this.elements.thumbnails.querySelector(`[data-target="${ event.detail.currentElement.dataset.mediaId }"]`);
       this.setActiveThumbnail(thumbnail);
     }
